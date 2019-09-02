485a61da62b0b94bc242d3ecae665508ac5a1663
Nurbek <nurbek-2395@mail.ru>
2019-09-02 16:04:53 +0600

    implement CommentPage.test.js

--- src/modules/CommentsPage/CommentsPage.js
+++ src/modules/CommentsPage/CommentsPage.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getComments } from "../../api";
+import { List, ListItem, ListItemText } from "@material-ui/core";
 
 export default function CommentsPage() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments()
+      .then(setComments)
+      .catch(console.error);
+  }, []);
   return (
     <div>
       <h1>Comments</h1>
+      <List>
+        {comments.map(comment => {
+          return (
+            <ListItem key={comment.id} divider>
+              <ListItemText
+                primary={comment.name}
+                secondary={`User: ${comment.email}`}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
     </div>
   );
 }
