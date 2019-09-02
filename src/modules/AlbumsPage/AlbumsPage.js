7e8f9f5c585c6039cbf73c05e716148a25a76466
Nurbek <nurbek-2395@mail.ru>
2019-09-02 15:56:51 +0600

    implementation of albumTest.js

--- src/modules/AlbumsPage/AlbumsPage.js
+++ src/modules/AlbumsPage/AlbumsPage.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Typography, List, ListItem, ListItemText } from "@material-ui/core";
+import { getAlbums } from "../../api";
 
 export default function AlbumsPage() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    getAlbums()
+      .then(setAlbums)
+      .catch(console.error);
+  }, []);
   return (
     <div>
-      <h1>Albums</h1>
+      <Typography variant="h2">Albums</Typography>
+      <List>
+        {albums.map(album => {
+          return (
+            <ListItem key={album.id} divider>
+              <ListItemText
+                primary={album.title}
+                secondary={`User: ${album.userId}`}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
     </div>
   );
 }
