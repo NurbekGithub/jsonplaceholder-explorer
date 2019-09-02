45a6711e2056e8bf22ea2faa11cb50dd6f935b9b
Nurbek <nurbek-2395@mail.ru>
2019-09-02 19:59:49 +0600

    some adjustments to homePage

--- src/modules/HomePage/HomePage.js
+++ src/modules/HomePage/HomePage.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to jsonplaceholder</h1>
+      <Typography variant="h3" align="center">
+        Welcome to jsonplaceholder explorer app which was written using React
+        and tdd aproach by{" "}
+        <a href="https://github.com/NurbekGithub">Nurbek Izbassar</a>
+      </Typography>
+      <Typography variant="h4" align="center">
+        <a href="https://github.com/NurbekGithub/jsonplaceholder-explorer">
+          Checkout source
+        </a>
+      </Typography>
     </div>
   );
 }
